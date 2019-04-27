bfb39abcedfbbb1c26b722b23f0411a5a9bc4b56
vonNiklasson <johan@nicklassons.se>
2019-04-27 17:56:36 +0200

    Updated models

---
+++ GraphServer/Pool.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace GraphServer;
+
+use GraphServer\Base\Pool as BasePool;
+
+/**
+ * Skeleton subclass for representing a row from the 'pool' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class Pool extends BasePool
+{
+
+}
