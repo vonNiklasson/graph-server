ee48132e8d26b63f4d5e969b3ad5a7b1c996a245
vonNiklasson <johan@nicklassons.se>
2019-04-27 17:43:40 +0200

    Built models

---
+++ GraphServer/Graph.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace GraphServer;
+
+use GraphServer\Base\Graph as BaseGraph;
+
+/**
+ * Skeleton subclass for representing a row from the 'graph' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class Graph extends BaseGraph
+{
+
+}
