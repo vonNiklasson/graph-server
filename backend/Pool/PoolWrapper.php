f12f4c392d95920fddf41b7cdf2c0db6bc003346
vonNiklasson <johan@nicklassons.se>
2019-04-28 17:18:18 +0200

    Fixed dict names

--- backend/Pool/PoolWrapper.php
+++ backend/Pool/PoolWrapper.php
@@ -76,19 +76,19 @@ class PoolWrapper {
         }
 
 
-        if (array_key_exists('nodes', $body)) {
+        if (array_key_exists('Nodes', $body)) {
             $nodeData = new WorkerData();
             $nodeData->setData($body['nodes']);
             $worker->setNodes($nodeData);
         }
 
-        if (array_key_exists('edges', $body)) {
+        if (array_key_exists('Edges', $body)) {
             $edgeData = new WorkerData();
             $edgeData->setData($body['edges']);
             $worker->setEdges($edgeData);
         }
 
-        if (array_key_exists('eccentricities', $body)) {
+        if (array_key_exists('Eccentricities', $body)) {
             $eccentricities = new WorkerData();
             $eccentricities->setData($body['eccentricities']);
             $worker->setEccentricities($eccentricities);
