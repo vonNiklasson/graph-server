245d49dc436b6d83ec6e56ac40886bd66d4a17cd
vonNiklasson <johan@nicklassons.se>
2019-04-29 09:38:07 +0200

    Added worker log to html

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,8 @@
 <?php
 
+use GraphServer\Map\WorkerTableMap;
 use GraphServer\PoolQuery;
+use GraphServer\WorkerQuery;
 
 require_once(__DIR__ . '/../backend/config.php');
 
@@ -61,6 +63,46 @@ require_once(__DIR__ . '/../backend/config.php');
 
         </tbody>
     </table>
+
+
+
+  <h3>Latest workers</h3>
+  <table class="table table-striped table-hover table-sm">
+    <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Created</th>
+      <th scope="col">Client</th>
+      <th scope="col">Nodes</th>
+      <th scope="col">State</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    $workers = WorkerQuery::create()->filterByOptimization('combined')->orderByCreatedTs()->find();
+    foreach ($workers as $worker) {
+        echo '<tr>';
+        echo '<th scope="row">' . $worker->getId() . '</th>';
+        echo '<td>' . date('h:i:s', $worker->getCreatedTs()) . '</td>';
+        echo '<td>' . $worker->getWorkerName() . '</td>';
+        echo '<td>' . $worker->getNodeCount() . '</td>';
+        echo '<td>';
+        if ($worker->getState() != WorkerTableMap::COL_STATE_IN_PROGRESS) {
+            echo 'In progress';
+        } elseif ($worker->getState() != WorkerTableMap::COL_STATE_DONE) {
+            echo 'Finished';
+            $delta = $worker->getClosedTs() - $worker->getCreatedTs();
+            echo ' (' . date('i:s', $delta) . ')';
+        } elseif ($worker->getState() != WorkerTableMap::COL_STATE_DEAD) {
+            echo 'Dead';
+        }
+        echo '</td>';
+        echo '</tr>';
+    }
+    ?>
+
+    </tbody>
+  </table>
 </div>
 
 
