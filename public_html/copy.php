9190df20253df9062ba0b428370cd8fd99c32527
vonNiklasson <johan@nicklassons.se>
2019-05-10 15:04:06 +0200

    Added copy script

---
+++ public_html/copy.php
@@ -0,0 +1,48 @@
+<?php
+
+use GraphServer\Map\WorkerTableMap;
+use GraphServer\PoolQuery;
+use GraphServer\WorkerQuery;
+
+require_once(__DIR__ . '/../backend/config.php');
+
+$pools = PoolQuery::create()
+    ->filterBySolveType('sfield')
+    ->orderByNodeCount()
+    ->find();
+
+ob_end_flush();
+foreach ($pools as $oldPool) {
+
+    # Get all the old workers
+    $oldWorkers = WorkerQuery::create()
+        ->filterByState(WorkerTableMap::COL_STATE_DONE)
+        ->filterByPool($oldPool)
+        ->find();
+
+    # Get the new related pool
+    $newPool = PoolQuery::create()
+        ->filterBySolveType('sfield_sr')
+        ->filterByNodeCount($oldPool->getNodeCount())
+        ->findOne();
+
+    echo 'Copying workers from pool ' . $oldPool->getId() . ' to ' . $newPool->getId() . " results";
+    flush();
+
+    # Iterate over all the old workers
+    foreach ($oldWorkers as $oldWorker) {
+        # Create a new worker from the old one
+        $newWorker = $oldWorker->copy();
+
+        $newWorker->setEdgeData($oldWorker->getEdgeData(true));
+        $newWorker->setNodeData($oldWorker->getNodeData(true));
+        $newWorker->setEccentricityData($oldWorker->getEccentricityData(true));
+        $newWorker->setCustomData('{}');
+        $newWorker->setPool($newPool);
+        $newWorker->save();
+    }
+
+    echo " - Done<br />\r\n";
+    flush();
+}
+ob_start();
