abfffa31094ae04c7a6b694e52d3ba30981b6414
vonNiklasson <johan@nicklassons.se>
2019-05-10 15:41:27 +0200

    Fixed result page

--- public_html/results.php
+++ public_html/results.php
@@ -36,391 +36,146 @@ function print_calc_obj($calc) {
     </div>
 </div>
 
-<h2>Static area</h2>
-<table class="table table-striped table-hover table-sm">
+<?php
+function print_table($solve_type, $name) {
+?>
+  <h2><?= $name ?></h2>
+  <table class="table table-striped table-hover table-sm">
     <thead class="thead-dark">
     <tr>
-        <th scope="col" colspan="3"></th>
+      <th scope="col" colspan="3"></th>
 
-        <th scope="col" colspan="4">Edge count</th>
-        <th scope="col" colspan="4">Convergence rate</th>
-        <th scope="col" colspan="4">Edge cost</th>
-        <th scope="col" colspan="4">Energy</th>
-        <th scope="col" colspan="4">Diameter</th>
-        <th scope="col" colspan="4">Avg. eccentricity</th>
+      <th scope="col" colspan="4">Edge count</th>
+      <th scope="col" colspan="4">Convergence rate</th>
+      <th scope="col" colspan="4">Edge cost</th>
+      <th scope="col" colspan="4">Energy</th>
+      <th scope="col" colspan="4">Diameter</th>
+      <th scope="col" colspan="4">Avg. eccentricity</th>
 
-        <th scope="col" colspan="4">Units covered</th>
-        <th scope="col" colspan="4">Units uncovered</th>
-        <th scope="col" colspan="4">Units overlap</th>
-        <th scope="col" colspan="4">Units overlap (acc)</th>
-        <th scope="col" colspan="4">Avg. detection speed</th>
-        <th scope="col" colspan="4">Coverage</th>
+      <th scope="col" colspan="4">Units covered</th>
+      <th scope="col" colspan="4">Units uncovered</th>
+      <th scope="col" colspan="4">Units overlap</th>
+      <th scope="col" colspan="4">Units overlap (acc)</th>
+      <th scope="col" colspan="4">Avg. detection speed</th>
+      <th scope="col" colspan="4">Coverage</th>
 
     </tr>
     <tr>
-        <th scope="col">#</th>
-        <th scope="col">Name</th>
-        <th scope="col">Nodes</th>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Nodes</th>
 
-        <th scope="col">Min</th>
-        <th scope="col">Max</th>
-        <th scope="col">Avg.</th>
-        <th scope="col">Med.</th>
+      <th scope="col">Min</th>
+      <th scope="col">Max</th>
+      <th scope="col">Avg.</th>
+      <th scope="col">Med.</th>
 
-        <th scope="col">Min</th>
-        <th scope="col">Max</th>
-        <th scope="col">Avg.</th>
-        <th scope="col">Med.</th>
+      <th scope="col">Min</th>
+      <th scope="col">Max</th>
+      <th scope="col">Avg.</th>
+      <th scope="col">Med.</th>
 
-        <th scope="col">Min</th>
-        <th scope="col">Max</th>
-        <th scope="col">Avg.</th>
-        <th scope="col">Med.</th>
+      <th scope="col">Min</th>
+      <th scope="col">Max</th>
+      <th scope="col">Avg.</th>
+      <th scope="col">Med.</th>
 
-        <th scope="col">Min</th>
-        <th scope="col">Max</th>
-        <th scope="col">Avg.</th>
-        <th scope="col">Med.</th>
+      <th scope="col">Min</th>
+      <th scope="col">Max</th>
+      <th scope="col">Avg.</th>
+      <th scope="col">Med.</th>
 
-        <th scope="col">Min</th>
-        <th scope="col">Max</th>
-        <th scope="col">Avg.</th>
-        <th scope="col">Med.</th>
+      <th scope="col">Min</th>
+      <th scope="col">Max</th>
+      <th scope="col">Avg.</th>
+      <th scope="col">Med.</th>
 
-        <th scope="col">Min</th>
-        <th scope="col">Max</th>
-        <th scope="col">Avg.</th>
-        <th scope="col">Med.</th>
+      <th scope="col">Min</th>
+      <th scope="col">Max</th>
+      <th scope="col">Avg.</th>
+      <th scope="col">Med.</th>
 
-        <th scope="col">Min</th>
-        <th scope="col">Max</th>
-        <th scope="col">Avg.</th>
-        <th scope="col">Med.</th>
+      <th scope="col">Min</th>
+      <th scope="col">Max</th>
+      <th scope="col">Avg.</th>
+      <th scope="col">Med.</th>
 
-        <th scope="col">Min</th>
-        <th scope="col">Max</th>
-        <th scope="col">Avg.</th>
-        <th scope="col">Med.</th>
+      <th scope="col">Min</th>
+      <th scope="col">Max</th>
+      <th scope="col">Avg.</th>
+      <th scope="col">Med.</th>
 
-        <th scope="col">Min</th>
-        <th scope="col">Max</th>
-        <th scope="col">Avg.</th>
-        <th scope="col">Med.</th>
+      <th scope="col">Min</th>
+      <th scope="col">Max</th>
+      <th scope="col">Avg.</th>
+      <th scope="col">Med.</th>
 
-        <th scope="col">Min</th>
-        <th scope="col">Max</th>
-        <th scope="col">Avg.</th>
-        <th scope="col">Med.</th>
+      <th scope="col">Min</th>
+      <th scope="col">Max</th>
+      <th scope="col">Avg.</th>
+      <th scope="col">Med.</th>
 
-        <th scope="col">Min</th>
-        <th scope="col">Max</th>
-        <th scope="col">Avg.</th>
-        <th scope="col">Med.</th>
+      <th scope="col">Min</th>
+      <th scope="col">Max</th>
+      <th scope="col">Avg.</th>
+      <th scope="col">Med.</th>
 
-        <th scope="col">Min</th>
-        <th scope="col">Max</th>
-        <th scope="col">Avg.</th>
-        <th scope="col">Med.</th>
+      <th scope="col">Min</th>
+      <th scope="col">Max</th>
+      <th scope="col">Avg.</th>
+      <th scope="col">Med.</th>
 
     </tr>
     </thead>
     <tbody>
 
     <?php
-        $pools = PoolQuery::create()
-            ->orderByNodeCount()
-            ->filterBySolveType('field')
-            ->filterByActive(true)
-            ->orderByNodeCount()
-            ->find();
+    $pools = PoolQuery::create()
+        ->orderByNodeCount()
+        ->filterBySolveType($solve_type)
+        ->filterByActive(true)
+        ->orderByNodeCount()
+        ->find();
 
-        foreach ($pools as $pool) {
-            $raw_results = $pool->getResults();
-            $results = json_decode($raw_results);
+    foreach ($pools as $pool) {
+        $raw_results = $pool->getResults();
+        $results = json_decode($raw_results);
 
-            if (strlen($raw_results) == 0) continue;
+        if (strlen($raw_results) == 0) continue;
 
-            echo '<tr>';
-                echo '<th scope="row">' . $pool->getId() . '</th>';
-                echo '<td>' . $pool->getName() . '</td>';
-                echo '<td>' . $pool->getNodeCount() . '</td>';
+        echo '<tr>';
+        echo '<th scope="row">' . $pool->getId() . '</th>';
+        echo '<td>' . $pool->getName() . '</td>';
+        echo '<td>' . $pool->getNodeCount() . '</td>';
 
-                print_calc_obj($results->edge_count);
-                print_calc_obj($results->convergence_rate);
-                print_calc_obj($results->edge_cost);
-                print_calc_obj($results->energy);
-                print_calc_obj($results->diameter);
-                print_calc_obj($results->avg_eccentricity);
+        print_calc_obj($results->edge_count);
+        print_calc_obj($results->convergence_rate);
+        print_calc_obj($results->edge_cost);
+        print_calc_obj($results->energy);
+        print_calc_obj($results->diameter);
+        print_calc_obj($results->avg_eccentricity);
 
-                print_calc_obj($results->units_covered);
-                print_calc_obj($results->units_uncovered);
-                print_calc_obj($results->units_overlap);
-                print_calc_obj($results->units_overlap_acc);
-                print_calc_obj($results->avg_detection_speed);
-                print_calc_obj($results->coverage);
-            echo '</tr>';
-        }
+        print_calc_obj($results->units_covered);
+        print_calc_obj($results->units_uncovered);
+        print_calc_obj($results->units_overlap);
+        print_calc_obj($results->units_overlap_acc);
+        print_calc_obj($results->avg_detection_speed);
+        print_calc_obj($results->coverage);
+        echo '</tr>';
+    }
     ?>
     </tbody>
-</table>
+  </table>
+
+    <?php
+}
 
 
-<h2>Dynamic area</h2>
-<table class="table table-striped table-hover table-sm">
-  <thead class="thead-dark">
-  <tr>
-    <th scope="col" colspan="3"></th>
-
-    <th scope="col" colspan="4">Edge count</th>
-    <th scope="col" colspan="4">Convergence rate</th>
-    <th scope="col" colspan="4">Edge cost</th>
-    <th scope="col" colspan="4">Energy</th>
-    <th scope="col" colspan="4">Diameter</th>
-    <th scope="col" colspan="4">Avg. eccentricity</th>
-
-    <th scope="col" colspan="4">Units covered</th>
-    <th scope="col" colspan="4">Units uncovered</th>
-    <th scope="col" colspan="4">Units overlap</th>
-    <th scope="col" colspan="4">Units overlap (acc)</th>
-    <th scope="col" colspan="4">Avg. detection speed</th>
-    <th scope="col" colspan="4">Coverage</th>
-
-  </tr>
-  <tr>
-    <th scope="col">#</th>
-    <th scope="col">Name</th>
-    <th scope="col">Nodes</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-  </tr>
-  </thead>
-  <tbody>
-
-  <?php
-  $pools = PoolQuery::create()
-      ->orderByNodeCount()
-      ->filterBySolveType('dfield')
-      ->filterByActive(true)
-      ->orderByNodeCount()
-      ->find();
-
-  foreach ($pools as $pool) {
-      $raw_results = $pool->getResults();
-      $results = json_decode($raw_results);
-
-      if (strlen($raw_results) == 0) continue;
-
-      echo '<tr>';
-      echo '<th scope="row">' . $pool->getId() . '</th>';
-      echo '<td>' . $pool->getName() . '</td>';
-      echo '<td>' . $pool->getNodeCount() . '</td>';
-
-      print_calc_obj($results->edge_count);
-      print_calc_obj($results->convergence_rate);
-      print_calc_obj($results->edge_cost);
-      print_calc_obj($results->energy);
-      print_calc_obj($results->diameter);
-      print_calc_obj($results->avg_eccentricity);
-
-      print_calc_obj($results->units_covered);
-      print_calc_obj($results->units_uncovered);
-      print_calc_obj($results->units_overlap);
-      print_calc_obj($results->units_overlap_acc);
-      print_calc_obj($results->avg_detection_speed);
-      print_calc_obj($results->coverage);
-      echo '</tr>';
-  }
-  ?>
-  </tbody>
-</table>
-
-
-<h2>Fixed spec area</h2>
-<table class="table table-striped table-hover table-sm">
-  <thead class="thead-dark">
-  <tr>
-    <th scope="col" colspan="3"></th>
-
-    <th scope="col" colspan="4">Edge count</th>
-    <th scope="col" colspan="4">Convergence rate</th>
-    <th scope="col" colspan="4">Edge cost</th>
-    <th scope="col" colspan="4">Energy</th>
-    <th scope="col" colspan="4">Diameter</th>
-    <th scope="col" colspan="4">Avg. eccentricity</th>
-
-    <th scope="col" colspan="4">Units covered</th>
-    <th scope="col" colspan="4">Units uncovered</th>
-    <th scope="col" colspan="4">Units overlap</th>
-    <th scope="col" colspan="4">Units overlap (acc)</th>
-    <th scope="col" colspan="4">Avg. detection speed</th>
-    <th scope="col" colspan="4">Coverage</th>
-
-  </tr>
-  <tr>
-    <th scope="col">#</th>
-    <th scope="col">Name</th>
-    <th scope="col">Nodes</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-    <th scope="col">Min</th>
-    <th scope="col">Max</th>
-    <th scope="col">Avg.</th>
-    <th scope="col">Med.</th>
-
-  </tr>
-  </thead>
-  <tbody>
-
-  <?php
-  $pools = PoolQuery::create()
-      ->orderByNodeCount()
-      ->filterBySolveType('sfield')
-      ->filterByActive(true)
-      ->orderByNodeCount()
-      ->find();
-
-  foreach ($pools as $pool) {
-      $raw_results = $pool->getResults();
-      $results = json_decode($raw_results);
-
-      if (strlen($raw_results) == 0) continue;
-
-      echo '<tr>';
-      echo '<th scope="row">' . $pool->getId() . '</th>';
-      echo '<td>' . $pool->getName() . '</td>';
-      echo '<td>' . $pool->getNodeCount() . '</td>';
-
-      print_calc_obj($results->edge_count);
-      print_calc_obj($results->convergence_rate);
-      print_calc_obj($results->edge_cost);
-      print_calc_obj($results->energy);
-      print_calc_obj($results->diameter);
-      print_calc_obj($results->avg_eccentricity);
-
-      print_calc_obj($results->units_covered);
-      print_calc_obj($results->units_uncovered);
-      print_calc_obj($results->units_overlap);
-      print_calc_obj($results->units_overlap_acc);
-      print_calc_obj($results->avg_detection_speed);
-      print_calc_obj($results->coverage);
-      echo '</tr>';
-  }
-  ?>
-  </tbody>
-</table>
+print_table('field', 'Static area');
+print_table('dfield', 'Dynamic area');
+print_table('sfield', 'Static area with specified nodes and growing radius');
+print_table('sfield_fr', 'Static area with specified nodes');
+?>
 
 
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
